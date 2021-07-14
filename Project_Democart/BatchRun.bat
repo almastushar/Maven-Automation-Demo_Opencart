4df4e8372a46cc4dc9d9e0f1cda8905b6f6f5062
almas-hossain <72722023+almas-hossain@users.noreply.github.com>
2021-07-15 00:53:12 +0600

    bat up

--- Project_Democart/BatchRun.bat
+++ Project_Democart/BatchRun.bat
@@ -1,16 +1,9 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE suite SYSTEM "https://testng.org/testng-1.0.dtd">
-<suite name="Suite">
-  <test thread-count="5" name="Test">
-    <classes>
-      <class name="pagefactory.com.tests.PagefactoryTests_OrderHistory_Transaction_Download"/>
-      <class name="pagefactory.com.tests.PagefactoryTests_Cartlist"/>
-      <class name="pagefactory.com.tests.PagefactoryTests_AddCart_From_Wishlist"/>
-      <class name="pagefactory.com.tests.PagefactoryTests_Count_Product"/>
-      <class name="pagefactory.com.tests.PagefactoryTests_Search"/>
-      <class name="pagefactory.com.tests.PagefactoryTests_Login"/>
-      <class name="pagefactory.com.tests.PagefactoryTests_Wishlist"/>
-      <class name="pagefactory.com.tests.PagefactoryTests_Reg"/>
-    </classes>
-  </test> <!-- Test -->
-</suite> <!-- Suite -->
+set projectLocation=C:\Users\User\AppData\Local\Jenkins\.jenkins\workspace\SeleniumTest
+ 
+cd %projectLocation%
+ 
+set classpath=%projectLocation%\bin;%projectLocation%\lib\*
+ 
+java org.testng.TestNG %projectLocation%\testng.xml
+ 
+pause
